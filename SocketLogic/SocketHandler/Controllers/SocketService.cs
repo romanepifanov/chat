20cb62a38b516a04d5fc68d453ec30170f622ded
Roman Epifanov <roman@softwave.co.il>
2020-07-02 15:11:32 +0300

    added back-end side logic

---
+++ SocketLogic/SocketHandler/Controllers/SocketService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SocketHandler.Controllers
+{
+    class SocketService
+    {
+        private NetworkStream _stream;
+        public SocketService(NetworkStream stream)
+        {
+            _stream = stream;
+        }
+
+        public void CheckConnnect(string message)
+        {
+            string swk = Regex.Match(message, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
+            string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+            byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
+            string swkaSha1Base64 = Convert.ToBase64String(swkaSha1);
+
+            // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+            byte[] response = Encoding.UTF8.GetBytes(
+                "HTTP/1.1 101 Switching Protocols\r\n" +
+                "Connection: Upgrade\r\n" +
+                "Upgrade: websocket\r\n" +
+                "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
+
+            _stream.Write(response, 0, response.Length);
+        }
+    }
+}
--- SocketLogic/SocketHandler/Program.cs
+++ SocketLogic/SocketHandler/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using SocketHandler.Controllers;
+using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SocketHandler
 {
@@ -7,16 +11,70 @@ namespace SocketHandler
     {
         static void Main()
         {
-            TcpListener listener = new TcpListener(1000);
-            listener.Start();
+            string ip = "127.0.0.1";
+            int port = 80;
+            var server = new TcpListener(IPAddress.Parse(ip), port);
+
+            server.Start();
+            Console.WriteLine("Server has started on {0}:{1}, Waiting for a connection...", ip, port);
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("A client connected.");
+
+            NetworkStream stream = client.GetStream();
+            SocketService socketService = new SocketService(stream);
 
             while (true)
             {
-                TcpClient client = listener.AcceptTcpClient();
-                NetworkStream stream = client.GetStream();
-                string date = DateTime.Now.ToLongDateString();
-                byte[] buf = System.Text.Encoding.ASCII.GetBytes(date);
-                stream.Write(buf, 0, date.Length);
+                while (!stream.DataAvailable) ;
+                while (client.Available < 3) ;
+
+                byte[] bytes = new byte[client.Available];
+                stream.Read(bytes, 0, client.Available);
+                string message = Encoding.UTF8.GetString(bytes);
+
+                if (Regex.IsMatch(message, "^GET", RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("=====Handshaking from client=====\n{0}", message);
+                    socketService.CheckConnnect(message);
+                }
+                else
+                {
+                    bool mask = (bytes[1] & 0b10000000) != 0;
+                    int msglen = bytes[1] - 128;
+                    int offset = 2;
+
+                    if (msglen == 126)
+                    {
+                        msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
+                        offset = 4;
+                    }
+
+                    if (msglen == 0)
+                    {
+                        Console.WriteLine("msglen == 0");
+                    }
+                    else if (mask)
+                    {
+                        byte[] decoded = new byte[msglen];
+                        byte[] masks = new byte[4] { bytes[offset], bytes[offset + 1], bytes[offset + 2], bytes[offset + 3] };
+                        offset += 4;
+
+                        for (int i = 0; i < msglen; ++i)
+                        {
+                            decoded[i] = (byte)(bytes[offset + i] ^ masks[i % 4]);
+                        }
+
+                        string text = Encoding.UTF8.GetString(decoded);
+                        Console.WriteLine("{0}", text);
+                    }
+                    else
+                    {
+                        Console.WriteLine("mask bit not set");
+                    }
+
+                    Console.WriteLine();
+                }
             }
         }
     }
