0723f66ab05db4a7b3c11035abb8bc3c0e066ea0
Roman Epifanov <roman@softwave.co.il>
2020-07-02 14:25:25 +0300

    added socket handler

---
+++ SocketHandler/SocketHandler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace SocketHandler
+{
+    class Program
+    {
+        static void Main()
+        {
+            TcpListener listener = new TcpListener(1000);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                string date = DateTime.Now.ToLongDateString();
+                byte[] buf = System.Text.Encoding.ASCII.GetBytes(date);
+                stream.Write(buf, 0, date.Length);
+            }
+        }
+    }
+}
